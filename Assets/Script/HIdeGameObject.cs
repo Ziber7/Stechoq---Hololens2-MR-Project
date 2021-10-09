073e8bc53c3afc2cd621a814f05777266f0d7998
Ziber7 <naufaljundi.2019@student.uny.ac.id>
2021-10-09 15:20:25 +0700

    push

---
+++ Assets/Script/HIdeGameObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HIdeGameObject : MonoBehaviour
+{
+    public GameObject object1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Sembunyikan()
+    {
+        StartCoroutine(DelaySeconds());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DelaySeconds()
+    {
+        yield return new WaitForSeconds(2);
+        object1.SetActive(false);
+    }
+}
